9e7dff77733cc9245906ea5b9e1df6ddc1a592fd
Smooth-E <smoothie@disroot.org>
2023-03-27 02:51:46 -0700

    Make carousels responsive to mobile screens

--- assets/js/products-carousel.js
+++ assets/js/products-carousel.js
@@ -1,29 +1,32 @@
 let margin = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue("--margin"))
 let elements = document.getElementsByClassName("carousel")
 
+function getScrollDelta(pane) {
+    return pane.querySelector(".card").offsetWidth + margin * 2
+}
+
 for (let i = 0; i < elements.length; i++) {
     let carousel = elements[i]
     let arrowLeft = carousel.querySelector("#arrow-left")
     let arrowRight = carousel.querySelector("#arrow-right")
     let pane = carousel.querySelector("#carousel-pane")
-    let scrollDelta = pane.querySelector(".card").offsetWidth + margin * 2
     let entryCount = parseInt(pane.dataset.entryCount)
 
-    arrowLeft.addEventListener("click", (event) => {
+    arrowRight.addEventListener("click", (event) => {
         let currentEntry = parseInt(pane.dataset.currentEntry) + 1
         if (currentEntry >= entryCount)
             currentEntry = 0
 
         pane.dataset.currentEntry = currentEntry.toString()
-        pane.style.transform = "translateX(" + currentEntry * -scrollDelta + "px)"
+        pane.style.transform = "translateX(" + currentEntry * -getScrollDelta(pane) + "px)"
     })
 
-    arrowRight.addEventListener("click", (event) => {
+    arrowLeft.addEventListener("click", (event) => {
         let currentEntry = parseInt(pane.dataset.currentEntry) - 1
         if (currentEntry < 0)
             currentEntry = entryCount - 1
 
         pane.dataset.currentEntry = currentEntry.toString()
-        pane.style.transform = "translateX(" + currentEntry * -scrollDelta + "px)"
+        pane.style.transform = "translateX(" + currentEntry * -getScrollDelta(pane) + "px)"
     })
 }
