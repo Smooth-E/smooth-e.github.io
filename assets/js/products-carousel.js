c1240e904c77940b8c46b0719774cb6e0f9b301a
Smooth-E <smoothie@disroot.org>
2023-03-28 08:19:29 -0700

    Add fly-in animations for carousels' cards

--- assets/js/products-carousel.js
+++ assets/js/products-carousel.js
@@ -5,12 +5,28 @@ function getScrollDelta(pane) {
     return pane.querySelector(".card").offsetWidth + margin * 2
 }
 
-for (let i = 0; i < elements.length; i++) {
-    let carousel = elements[i]
+for (let i = 1; i <= elements.length; i++) {
+    let carousel = elements[i - 1]
     let arrowLeft = carousel.querySelector("#arrow-left")
     let arrowRight = carousel.querySelector("#arrow-right")
     let pane = carousel.querySelector("#carousel-pane")
     let entryCount = parseInt(pane.dataset.entryCount)
+
+    for (let cardIndex = 1; cardIndex <= entryCount; cardIndex++) {
+        let styleElement = document.createElement("style")
+        styleElement.innerText = `
+            .card:nth-child(${cardIndex}) {
+                position: relative;
+                animation-name: card-appearance;
+                animation-duration: 1s;
+                left: ${500 * cardIndex}px;
+                top: ${50 * cardIndex}px;
+                animation-timing-function: ease;
+                animation-fill-mode: forwards;
+            }
+        `
+        carousel.appendChild(styleElement)
+    }
 
     arrowRight.addEventListener("click", (event) => {
         let currentEntry = parseInt(pane.dataset.currentEntry) + 1
