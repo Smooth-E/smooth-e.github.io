664d768df26ab884931383b2ed643de6ded84bef
Smooth-E <smoothie@disroot.org>
2023-03-28 10:06:16 -0700

    Use transform: translate(); for cards' animations to improve performance

--- assets/js/products-carousel.js
+++ assets/js/products-carousel.js
@@ -7,40 +7,47 @@ function getScrollDelta(pane) {
 
 for (let i = 1; i <= elements.length; i++) {
     let carousel = elements[i - 1]
-    let arrowLeft = carousel.querySelector("#arrow-left")
-    let arrowRight = carousel.querySelector("#arrow-right")
     let pane = carousel.querySelector("#carousel-pane")
-    let entryCount = parseInt(pane.dataset.entryCount)
+    let cards = carousel.querySelectorAll(".card")
+    let greatestHeight = 0
 
-    for (let cardIndex = 1; cardIndex <= entryCount; cardIndex++) {
+    for (let cardIndex = 1; cardIndex <= cards.length; cardIndex++) {
         let styleElement = document.createElement("style")
         styleElement.innerText = `
-            .card:nth-child(${cardIndex}) {
+            .container-wrapper:nth-child(${cardIndex}) {
                 position: relative;
                 animation-name: card-appearance;
                 animation-duration: 1s;
-                left: ${500 * cardIndex}px;
-                top: ${50 * cardIndex}px;
+                transform: translate(${500 * cardIndex}px, ${50 * cardIndex}px);
                 animation-timing-function: ease;
                 animation-fill-mode: forwards;
             }
         `
         carousel.appendChild(styleElement)
+
+        let height = cards[i - 1].offsetHeight
+        if (height > greatestHeight)
+            greatestHeight = height
     }
 
+    for (let index = 0; index < cards.length; index++)
+        cards[index].style.height = greatestHeight - margin * 2 + 'px'
+
+    let arrowRight = carousel.querySelector("#arrow-right")
     arrowRight.addEventListener("click", (event) => {
         let currentEntry = parseInt(pane.dataset.currentEntry) + 1
-        if (currentEntry >= entryCount)
+        if (currentEntry >= cards.length)
             currentEntry = 0
 
         pane.dataset.currentEntry = currentEntry.toString()
         pane.style.transform = "translateX(" + currentEntry * -getScrollDelta(pane) + "px)"
     })
 
+    let arrowLeft = carousel.querySelector("#arrow-left")
     arrowLeft.addEventListener("click", (event) => {
         let currentEntry = parseInt(pane.dataset.currentEntry) - 1
         if (currentEntry < 0)
-            currentEntry = entryCount - 1
+            currentEntry = cards.length - 1
 
         pane.dataset.currentEntry = currentEntry.toString()
         pane.style.transform = "translateX(" + currentEntry * -getScrollDelta(pane) + "px)"
